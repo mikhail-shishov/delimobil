e58e7b2d026c0ee5ad2436e75524e1e3f32d2ed9
mksh-su <shishovmike96@gmail.com>
2024-09-19 11:47:18 +0200

    Working suggestions

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
+    // увеличение инпута в зависимости от текста
     function commentResize(textarea) {
         textarea.style.height = "auto";
         textarea.style.height = textarea.scrollHeight + "px";
     }
 
-    document.querySelector(".form-input").addEventListener('input', function () {
+    const mainInput = document.querySelector(".form-input")
+    mainInput.addEventListener('input', function () {
         commentResize(this);
+    });
+
+    // шаги
+    const steps = document.querySelectorAll(".step");
+
+    // кнопки для инпута
+    const suggestions = document.querySelectorAll(".form-helper-btn")
+    suggestions.forEach(item => {
+        item.addEventListener("click", function (e) {
+            e.preventDefault();
+            let content = item.innerHTML;
+            mainInput.innerHTML += content + "&nbsp;";
+            commentResize(mainInput);
+        })
     });
 })
